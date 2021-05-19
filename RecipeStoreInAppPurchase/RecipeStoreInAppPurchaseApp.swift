2703193b8a2f0673be17c9d9e0dc25fd95069a59
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-19 12:29:30 +0300

    Initial Commit

---
+++ RecipeStoreInAppPurchase/RecipeStoreInAppPurchaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeStoreInAppPurchaseApp.swift
+//  RecipeStoreInAppPurchase
+//
+//  Created by ramil on 19.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct RecipeStoreInAppPurchaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
